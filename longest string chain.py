5c80cf3e3727c530c621a5a429223288bf5892b5
yanqi8 <yan.qi@yale.edu>
2018-02-06 12:47:48 -0500

    find the longest chain

--- longest string chain.py
+++ longest string chain.py
@@ -1,6 +1,6 @@
 w = [ "a",  "b",  "ba", "bca", "bda", "bdca" ]
 
-
+# mine version
 def find_next_chain(word, words):
 # for any string, find the descendants if any
     chains = set()
@@ -31,3 +31,40 @@ def get_max_chain(words):
     for i in words:
         chain_lengths.append(max_chain(i, words))
     return max(chain_lengths)
+
+
+
+
+
+# quicker version
+def longest(word_set, cache, word):
+    if word not in cache:
+        ret = 1
+        for i in xrange(len(word)):
+            w = word[:i] + word[i+1:]
+            if w and w in word_set:
+                cnt = longest(word_set, cache, w)
+                ret = max(ret, 1 + cnt)
+
+        cache[word] = ret
+
+    return cache[word]
+
+
+def longestChain(words):
+    cache = {}
+    word_set = set(words)
+    gmax = 0
+    for word in words:
+        gmax = max(gmax, longest(word_set, cache, word))
+
+    return gmax
+
+
+
+
+
+
+
+
+
